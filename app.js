a9f235bb6da7410c6bfa74c84d01c1f5ef38b4f7
metelskiwiktor <metelski.wiktor@gmail.com>
2026-04-08 11:35:01 +0200

    Add call to `generateWordle` during initialization

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('lenExact').value = 5;
   generate();
   initWordle();
+  generateWordle();
 });
 
 function addExcludePosition() {
